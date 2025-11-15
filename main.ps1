d3d0ea97384e04b6389ddba221286f6b23d0add4
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2025-11-15 23:20:36 +0100

    [DEV] -> Commentaire

---
+++ main.ps1
@@ -0,0 +1,21 @@
+<#
+
+.SYNOPSIS
+Deploiement de GPO automatisé 
+
+.PARAMETER GPO Name 
+Nom du GPO 
+
+.PARAMETER Path
+Chemin de l'unité ou du domaine lié a la gpo mise en place 
+
+.PARAMETER TemplateGPO 
+GPO à mettre en place 
+
+.PARAMETER Liste
+Liste des groupes/Utilisateurs 
+
+.PARAMETER WMI 
+Gestion des filtres WMI 
+
+#>
